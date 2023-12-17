f6907fd5f04e52efd149d65935a159d07da4b1ec
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2023-12-16 19:51:50 -0600

    test: Update router.test.ts with new navigation functions

--- test/router.test.ts
+++ test/router.test.ts
@@ -2,6 +2,8 @@ import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 import 'urlpattern-polyfill'
 
+import { TAG_NAME_ROUTER } from '../src/declarations.js'
+import { navigate } from '../src/utilities.js'
 import { RouteConfig } from '../src/route.js'
 import { LitRouter } from '../src/router.js'
 import '../src/router.js'
@@ -52,7 +54,7 @@ const routes: Partial<RouteConfig>[] = [
 ]
 
 beforeAll(async () => {
-  const $router = document.createElement('lit-router')
+  const $router = document.createElement(TAG_NAME_ROUTER)
 
   $router.setRoutes(routes)
 
@@ -62,126 +64,122 @@ beforeAll(async () => {
 })
 
 afterAll(() => {
-  const $router = document.querySelector('lit-router') as LitRouter
+  const $router = document.querySelector(TAG_NAME_ROUTER) as LitRouter
 
   document.body.removeChild($router)
 })
 
-describe('router', () => {
-  it('Check that the renderes component is home-page', async () => {
-    const $router = document.querySelector('lit-router') as LitRouter
+it('Check that the initial route is (/)', async () => {
+  const $router = document.querySelector(TAG_NAME_ROUTER) as LitRouter
 
-    const $homePage = $router.querySelector('home-page')
+  expect($router).toBeDefined()
 
-    expect(_stripExpressionComments($homePage!.shadowRoot!.innerHTML)).toBe('<h1>Home Page</h1>')
+  const $homePage = $router.querySelector('home-page')
+  expect(_stripExpressionComments($homePage!.shadowRoot!.innerHTML)).toBe('<h1>Home Page</h1>')
+})
+
+it('Check navigation() function', async () => {
+  const $router = document.querySelector(TAG_NAME_ROUTER) as LitRouter
+
+  $router.navigate({ path: '/about' })
+
+  await _delay(50)
+
+  expect($router.querySelector('home-page')).toBeNull()
+
+  const $aboutPage = $router.querySelector('about-page')
+  expect(_stripExpressionComments($aboutPage!.shadowRoot!.innerHTML)).toBe('<h1>About Page</h1>')
+
+  navigate({ path: '/' })
+
+  await _delay(50)
+
+  const $homePage = $router.querySelector('home-page')
+  expect(_stripExpressionComments($homePage!.shadowRoot!.innerHTML)).toBe('<h1>Home Page</h1>')
+})
+
+it('Check wildcard routes (404 not found)', async () => {
+  const $router = document.querySelector(TAG_NAME_ROUTER) as LitRouter
+
+  $router.navigate({ path: '/random' })
+
+  await _delay(50)
+
+  const $notFoundPage = $router.querySelector('notfound-page')
+
+  expect(_stripExpressionComments($notFoundPage!.shadowRoot!.innerHTML)).toBe('<h1>404 | Not Found</h1>')
+})
+
+it('Check the route guards', async () => {
+  const $router = document.querySelector(TAG_NAME_ROUTER) as LitRouter
+
+  $router.navigate({ path: '/dashboard' })
+
+  await _delay(50)
+
+  const $homePage = $router.querySelector('home-page')
+  expect(_stripExpressionComments($homePage!.shadowRoot!.innerHTML)).toBe('<h1>Home Page</h1>')
+
+  $router.navigate({
+    path: '/dashboard',
+    query: { token: '123' }
   })
 
-  it('Check that the renderes component is about-page', async () => {
-    const $router = document.querySelector('lit-router') as LitRouter
+  await _delay(50)
 
-    $router.navigate({ path: '/about' })
+  const $dashboardPage = $router.querySelector('dashboard-page')
+  expect(_stripExpressionComments($dashboardPage!.shadowRoot!.innerHTML)).toMatch(/Dashboard Page/)
 
-    await _delay(50)
-
-    const $aboutPage = $router.querySelector('about-page')
-
-    expect(_stripExpressionComments($aboutPage!.shadowRoot!.innerHTML)).toBe('<h1>About Page</h1>')
+  $router.navigate({
+    path: '/dashboard/users/4',
+    query: { token: '123' }
   })
 
-  it('Check that back() navigation works', async () => {
-    const $router = document.querySelector('lit-router') as LitRouter
+  await _delay(50)
 
-    $router.back()
+  const $usersPage = $router.querySelector('users-page')
 
-    await _delay(50)
+  expect($usersPage).toBeNull()
+  expect(_stripExpressionComments($homePage!.shadowRoot!.innerHTML)).toBe('<h1>Home Page</h1>')
+})
 
-    const $homePage = $router.querySelector('home-page')
+it('Check nested routes', async () => {
+  const $router = document.querySelector(TAG_NAME_ROUTER) as LitRouter
 
-    expect(_stripExpressionComments($homePage!.shadowRoot!.innerHTML)).toBe('<h1>Home Page</h1>')
+  $router.navigate({
+    path: '/dashboard/users/1',
+    query: { token: '123' }
   })
 
-  it('Check that forward() navigation works', async () => {
-    const $router = document.querySelector('lit-router') as LitRouter
+  await _delay(50)
 
-    $router.forward()
+  const $dashboardPage = $router.querySelector('dashboard-page')
+  const $usersPage = $router.querySelector('users-page')
 
-    await _delay(50)
+  expect(_stripExpressionComments($dashboardPage!.shadowRoot!.innerHTML)).toMatch(/<h1>Dashboard Page<\/h1>/)
+  expect(_stripExpressionComments($usersPage!.shadowRoot!.innerHTML)).toBe('<h1>Users Page 1</h1>')
+})
 
-    const $aboutPage = $router.querySelector('about-page')
+it('Check that back() navigation function', async () => {
+  const $router = document.querySelector(TAG_NAME_ROUTER) as LitRouter
 
-    expect(_stripExpressionComments($aboutPage!.shadowRoot!.innerHTML)).toBe('<h1>About Page</h1>')
-  })
+  $router.back()
 
-  it('Check that the renderes component is notfound-page', async () => {
-    const $router = document.querySelector('lit-router') as LitRouter
+  await _delay(50)
+  
+  const $dashboardPage = $router.querySelector('dashboard-page')
 
-    $router.navigate({ path: '/random' })
+  expect(_stripExpressionComments($dashboardPage!.shadowRoot!.innerHTML)).toMatch(/Dashboard Page/)
+})
 
-    await _delay(50)
+it('Check that forward() navigation function', async () => {
+  const $router = document.querySelector(TAG_NAME_ROUTER) as LitRouter
 
-    const $notFoundPage = $router.querySelector('notfound-page')
+  $router.forward()
 
-    expect(_stripExpressionComments($notFoundPage!.shadowRoot!.innerHTML)).toBe('<h1>404 | Not Found</h1>')
-  })
+  await _delay(50)
 
-  it('Check that the renderes component is not dashboard-page', async () => {
-    const $router = document.querySelector('lit-router') as LitRouter
+  const $usersPage = $router.querySelector('users-page')
 
-    $router.navigate({ path: '/dashboard' })
-
-    await _delay(50)
-
-    const $dashboardPage = $router.querySelector('dashboard-page')
-    expect($dashboardPage).toBeNull()
-
-    const $homePage = $router.querySelector('home-page')
-    expect(_stripExpressionComments($homePage!.shadowRoot!.innerHTML)).toBe('<h1>Home Page</h1>')
-  })
-
-  it('Check that the renderes component is dashboard-page', async () => {
-    const $router = document.querySelector('lit-router') as LitRouter
-
-    $router.navigate({
-      path: '/dashboard',
-      query: { token: '123' }
-    })
-
-    await _delay(50)
-
-    const $dashboardPage = $router.querySelector('dashboard-page')
-
-    expect(_stripExpressionComments($dashboardPage!.shadowRoot!.innerHTML)).toMatch(/Dashboard Page/)
-  })
-
-  it('Check that the renderes component is not users-page', async () => {
-    const $router = document.querySelector('lit-router') as LitRouter
-
-    $router.navigate({
-      path: '/dashboard/users/4',
-      query: { token: '123' }
-    })
-
-    await _delay(50)
-
-    const $usersPage = $router.querySelector('users-page')
-    expect($usersPage).toBeNull()
-
-    const $dashboardPage = $router.querySelector('dashboard-page')
-    expect(_stripExpressionComments($dashboardPage!.shadowRoot!.innerHTML)).toMatch(/Dashboard Page/)
-  })
-
-  it('Check that the renderes component is users-page', async () => {
-    const $router = document.querySelector('lit-router') as LitRouter
-
-    $router.navigate({
-      path: '/dashboard/users/1',
-      query: { token: '123' }
-    })
-
-    await _delay(50)
-
-    const $usersPage = $router.querySelector('users-page')
-
-    expect(_stripExpressionComments($usersPage!.shadowRoot!.innerHTML)).toBe('<h1>Users Page 1</h1>')
-  })
+  expect(_stripExpressionComments($usersPage!.shadowRoot!.innerHTML)).toBe('<h1>Users Page 1</h1>')
 })
