0f0c0c0cf7e97c69a8f2935a46c2187e538f9eb9
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2023-12-25 09:43:25 -0600

    Update product pictures and styles on dashboard page

--- demo/src/pages/dashboard.page.ts
+++ demo/src/pages/dashboard.page.ts
@@ -1,7 +1,12 @@
+import { customElement, state } from 'lit/decorators.js';
 import { LitElement, css, html, unsafeCSS } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { when } from 'lit/directives/when.js';
 
 import styles from './dashboard.page.css?inline';
+
+import { environments } from '../environments.js'
+
+const { BASE_URL_API } = environments
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -13,9 +18,55 @@ declare global {
 export class DashboardPage extends LitElement {
   static styles = css`${unsafeCSS(styles)}`;
 
+  @state() private _products: any[] = [];
+
+  connectedCallback (): void {
+    super.connectedCallback();
+    
+    this._fetchProducts()
+  }
+
+  private async _fetchProducts (): Promise<void> {
+    const url = `${BASE_URL_API}/products?_expand=categories`
+
+    const res = await fetch(url)
+
+    const products = await res.json()
+
+    this._products = products
+  }
+
   protected render (): unknown {
     return html`
-      <h1>Dashboard</h1>
+      <main>
+        <ul class="products">
+          ${this._products.map(product => html`
+            <li class="product">
+              <header class="product__header">
+                <img
+                  class="product__picture"
+                  src="${product.picture}"
+                  alt="${product.name}"
+                />
+              </header>
+
+              <main class="product__body">
+                <div class="product__highlight">
+                  <p class="product__price">$${product.price}</p>
+
+                  ${when(product.categories, () => html`
+                    <span class="product__chip">
+                      ${product.categories.name}
+                    </span>
+                  `)}
+                </div>
+                <h1 class="product__title">${product.name}</h1>
+                <p class="product__description">${product.description}</p>
+              </main>
+            </li>
+          `)}
+        </ul>
+      </main>
     `
   }
 }
